26a1c2f1bac159585724f5f95effbbcca8b30b0a
xujialiu <xujialiuphd@gmail.com>
2024-08-28 00:51:43 +0800

    feat: update trainer for testing

--- trainer.py
+++ trainer.py
@@ -3,11 +3,12 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
 from lightning.pytorch.strategies import DDPStrategy
 from lightningmodule import RETFoundLightning
+
 from datamodule import SingleImageDataModule
 
 
 checkpoint_callback = ModelCheckpoint(
-    monitor="val_acc",
+    monitor="val_auc_roc",
     filename="retfound-{epoch:02d}-{val_acc:.2f}",
     every_n_epochs=1,
     save_top_k=-1,
@@ -22,6 +23,21 @@ tensorboard_logger = TensorBoardLogger(save_dir="test_root", name="tensorboard")
 csv_logger = CSVLogger(save_dir="test_root", name="csv")
 loggers = [tensorboard_logger, csv_logger]
 
+# trainer = Trainer(
+#     devices=[0],
+#     accelerator="gpu",
+#     # strategy=DDPStrategy(find_unused_parameters=False),
+#     max_epochs=10,
+#     callbacks=[checkpoint_callback],
+#     logger=loggers,
+#     use_distributed_sampler=False,
+#     limit_train_batches=0.1,
+#     limit_val_batches=0.03,
+#     limit_test_batches=0.03,
+#     benchmark=True,
+#     precision="16-mixed",
+# )
+
 trainer = Trainer(
     devices=[1],
     accelerator="gpu",
@@ -29,15 +45,26 @@ trainer = Trainer(
     max_epochs=10,
     callbacks=[checkpoint_callback],
     logger=loggers,
-    use_distributed_sampler=True,
-    # Add these lines to skip sanity check and enable deterministic mode
+    use_distributed_sampler=False,
+    limit_train_batches=0.1,
+    limit_val_batches=0.03,
+    limit_test_batches=0.03,
+    benchmark=True,
+    precision="16-mixed",
 )
 
-model = RETFoundLightning()
 
+model = RETFoundLightning(
+    use_original_retfound_ckpt="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/my-model/other_models/model-checkpoints/RETFound.pth",
+    warmup_epochs=5,
+)
 datamodule = SingleImageDataModule(
     excel_file="data_cleansing/get_macula_table/macula_table.xlsx",
     data_folder="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/data_macula_all",
+    batch_size=6,
 )
 
-trainer.fit(model, datamodule=datamodule)
+trainer.fit(
+    model,
+    datamodule=datamodule,
+)
