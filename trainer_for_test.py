fb03bd80376adcde0b4eb16fee5466223a30794c
xujialiu <xujialiuphd@gmail.com>
2024-08-29 18:55:17 +0800

    feat: updated the trainer code for testing

--- trainer_for_test.py
+++ trainer_for_test.py
@@ -6,6 +6,9 @@ from lightningmodule import RETFoundLightning
 
 from datamodule import SingleImageDataModule
 
+INPUT_SIZE = 448
+WARMUP_EPOCHS = 10
+EPOCHS = 50
 
 checkpoint_callback = ModelCheckpoint(
     monitor="val_auc_roc",
@@ -18,21 +21,6 @@ tensorboard_logger = TensorBoardLogger(save_dir="test_root", name="tensorboard")
 csv_logger = CSVLogger(save_dir="test_root", name="csv", flush_logs_every_n_steps=20)
 loggers = [tensorboard_logger, csv_logger]
 
-# trainer = Trainer(
-#     devices=[0],
-#     accelerator="gpu",
-#     # strategy=DDPStrategy(find_unused_parameters=False),
-#     max_epochs=10,
-#     callbacks=[checkpoint_callback],
-#     logger=loggers,
-#     use_distributed_sampler=False,
-#     limit_train_batches=0.1,
-#     limit_val_batches=0.03,
-#     limit_test_batches=0.03,
-#     benchmark=True,
-#     precision="16-mixed",
-# )
-
 trainer = Trainer(
     devices=[1],
     accelerator="gpu",
@@ -40,6 +28,7 @@ trainer = Trainer(
     max_epochs=10,
     callbacks=[checkpoint_callback],
     logger=loggers,
+    accumulate_grad_batches=2,
     use_distributed_sampler=False,
     limit_train_batches=0.05,
     limit_val_batches=0.03,
@@ -50,7 +39,8 @@ trainer = Trainer(
 
 model = RETFoundLightning(
     use_original_retfound_ckpt="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/my-model/other_models/model-checkpoints/RETFound.pth",
-    warmup_epochs=5,
+    img_size=INPUT_SIZE,
+    warmup_epochs=WARMUP_EPOCHS,
 )
 datamodule = SingleImageDataModule(
     excel_file="data_cleansing/get_macula_table/macula_table.xlsx",
@@ -58,6 +48,7 @@ datamodule = SingleImageDataModule(
     label_col="label",
     data_folder="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/data_macula_all",
     batch_size=6,
+    input_size=INPUT_SIZE,
 )
 
 trainer.fit(
