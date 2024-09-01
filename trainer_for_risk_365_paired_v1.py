4cb3ed5d14f3f8e80b5596ebfd20e3ca98c71cac
xujialiu <xujialiuphd@gmail.com>
2024-09-01 18:48:46 +0800

    bug: disable sanity check to avoid plotting error confusion matrix fig.

--- trainer_for_risk_365_paired_v1.py
+++ trainer_for_risk_365_paired_v1.py
@@ -2,13 +2,16 @@ from lightning import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
 from lightning.pytorch.strategies import DDPStrategy
-from retfound_base_lightningmodule import RETFoundLightning
+from retfound_paired_lightningmodule import PairedRETFoundLightning
 
-from datamodule import SingleImageDataModule
+
+from datamodule import SingleImageDataModule, PairedImageDataModule
 
 INPUT_SIZE = 896
 WARMUP_EPOCHS = 10
 EPOCHS = 50
+BATCH_SIZE = 3
+ACCUMULATE_GRAD_BATCHES = 11
 
 checkpoint_callback = ModelCheckpoint(
     monitor="val_auc_roc",
@@ -17,8 +20,8 @@ checkpoint_callback = ModelCheckpoint(
     save_top_k=-1
 )
 
-tensorboard_logger = TensorBoardLogger(save_dir="./risk_365", name="tensorboard")
-csv_logger = CSVLogger(save_dir="./risk_365", name="csv", flush_logs_every_n_steps=20)
+tensorboard_logger = TensorBoardLogger(save_dir="./risk_365_paired", name="tensorboard")
+csv_logger = CSVLogger(save_dir="./risk_365_paired", name="csv", flush_logs_every_n_steps=20)
 loggers = [tensorboard_logger, csv_logger]
 
 trainer = Trainer(
@@ -28,7 +31,7 @@ trainer = Trainer(
     max_epochs=EPOCHS,
     callbacks=[checkpoint_callback],
     logger=[tensorboard_logger, csv_logger],
-    accumulate_grad_batches=5,
+    accumulate_grad_batches=ACCUMULATE_GRAD_BATCHES,
     use_distributed_sampler=False,
     benchmark=True,
     precision="16-mixed",
@@ -38,18 +41,21 @@ trainer = Trainer(
     # limit_test_batches=0.03,
 )
 
-model = RETFoundLightning(
+model = PairedRETFoundLightning(
     use_original_retfound_ckpt="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/my-model/other_models/model-checkpoints/RETFound.pth",
     img_size=INPUT_SIZE,
     warmup_epochs=WARMUP_EPOCHS,
     num_classes=2,
 )
-datamodule = SingleImageDataModule(
-    excel_file="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/data_cleansing/calculate time diff/macula_table_risk_level.xlsx",
-    img_path_col="macula_filename",
+
+datamodule = PairedImageDataModule(
+    excel_file="/mnt/4T/xujialiu/my_lightning_model/data_cleansing/calculate time diff/disc_macula_table_risk_level.xlsx",
+    macula_img_path_col="macula_filename",
+    disc_img_path_col = "disc_filename",
     label_col="label_365_diff_binary",
-    data_folder="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/data_macula_all",
-    batch_size=6,
+    macula_folder="/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/data_macula_all",
+    disc_folder = "/home/xujialiu/mnt-4T-xujialiu/my_lightning_model/xujialiu-mnt-4t/data_disc_all",
+    batch_size=BATCH_SIZE,
     input_size=INPUT_SIZE,
 )
 
@@ -58,4 +64,4 @@ trainer.fit(
     datamodule=datamodule,
 )
 
-# nohup /home/xujialiu/miniconda3/envs/pytorch/bin/python /mnt/4T/xujialiu-ckpt/my_lightning_model/trainer_for_risk_365_v1.py > risk_365_output.log 2>&1 &
+# nohup /mnt/4T/xujialiu/miniconda/envs/pt/bin/python /mnt/4T/xujialiu/my_lightning_model/trainer_for_risk_365_paired_v1.py > risk_365_paired_output.log 2>&1 &
